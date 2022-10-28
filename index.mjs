9c0cb25eb91ddfef2e4679236f4f5d4cc24fc3f4
Michael Kreil <github@michael-kreil.de>
2022-10-28 11:33:46 +0200

    add ETOPO1

--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
 'use strict'
 
+export * from './lib/ETOPO1.mjs'
 export * from './lib/ETOPO5.mjs'
--- test.js
+++ test.js
@@ -3,9 +3,9 @@
 start();
 
 async function start() {
-	const { ETOPO5 } = await import('./index.mjs');
-	let etopo5 = new ETOPO5();
-	//let elevation = await etopo5.getElevation(13.4083, 52.5186); // 44
-	//let elevation = await etopo5.getElevation(86.92483, 27.98787); // 8848
+	const { ETOPO1 } = await import('./index.mjs');
+	let etopo1 = new ETOPO1();
+	//let elevation = await etopo1.getElevation(13.4083, 52.5186); // 44
+	let elevation = await etopo1.getElevation(86.92483, 27.98787); // 8848
 	console.log(elevation);
 }
